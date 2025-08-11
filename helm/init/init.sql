3e27e0e4eeeb23e5ab9fee5f64fb52219a3bffaf
Igor Lukin <Readovec@mail.ru>
2025-08-11 18:23:28 +0300

    monitoring complitely fixed

--- helm/init/init.sql
+++ helm/init/init.sql
@@ -1,11 +1,29 @@
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(100) NOT NULL,
-            email VARCHAR(100) UNIQUE NOT NULL,
-            created_at TIMESTAMP DEFAULT NOW()
-        );
-        INSERT INTO users (username, email) VALUES
-            ('alice', 'alice@example.com'),
-            ('bob', 'bob@example.com'),
-            ('charlie', 'charlie@example.com')
-        ON CONFLICT (email) DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO users (username, email) VALUES
+    ('alice', 'alice@example.com'),
+    ('bob', 'bob@example.com'),
+    ('charlie', 'charlie@example.com')
+ON CONFLICT (email) DO NOTHING;
+
+DO
+$$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'exporter') THEN
+    CREATE ROLE exporter LOGIN PASSWORD 'Qzwxec123';
+  ELSE
+    ALTER ROLE exporter WITH PASSWORD 'Qzwxec123';
+  END IF;
+END
+$$;
+
+GRANT CONNECT ON DATABASE "Users" TO exporter;
+GRANT USAGE ON SCHEMA public TO exporter;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO exporter;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO exporter;
